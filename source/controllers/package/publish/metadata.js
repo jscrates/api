83c768869b2eab81823c3a686deecae115304d72
Jarvis2001 <moremakarand2001@gmail.com>
2021-12-14 23:29:39 +0530

    Joi package for metadata validation, DKPK check

--- source/controllers/package/publish/metadata.js
+++ source/controllers/package/publish/metadata.js
@@ -1,12 +1,35 @@
+const joi = require('joi')
+
 function publishPackageMetadata(req, res) {
-  const { metadata } = req.body
+  const { package_name, version, schema } = req.body
 
   // Validate the package metdata here
-  // if validation fails,
-  // return res.status(400).json({ message: `Invalid metadata!` })
+  const mdata = joi.object({
+    package_name: joi.string(),
+    version: joi.number(),
+    schema: joi.string().uri(),
+  })
+  const { error } = mdata.validate({ package_name, version, schema })
 
+  // if validation fails,
+  if (error) {
+    switch (error.detas[0].context.key) {
+      case 'package':
+        res.status(200).json({ message: error.details[0].message })
+        break
+      case 'version':
+        res.status(200).json({ message: error.details[0].message })
+        break
+      case 'schema':
+        res.status(200).json({ message: error.details[0].message })
+        break
+      default:
+        res.status(200).json({ message: 'Invalid metadata!' })
+        break
+    }
+  }
+  // return res.status(400).json({ message: `Invalid metadata!` })
   return res.status(200).json({ message: `Package published!` })
 }
 
 module.exports = publishPackageMetadata
-//all
