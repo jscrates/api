3543640dd65b30d80a5c637b355da4f361ee7bab
D. Kasi Pavan Kumar <44864604+kasipavankumar@users.noreply.github.com>
2021-12-20 19:11:57 +0000

    :alembic: Allow for multiple fields.
    We are now able to send both the package bundle & package meta file.

--- source/middleware/process-package.js
+++ source/middleware/process-package.js
@@ -1,10 +1,28 @@
 const multer = require('multer')
+const path = require('path')
+
+const messages = require('../lib/messages')
 
 const MAX_FILE_SIZE = 10 * 1024 * 1024
+
+// We only allow .tgz files as bundled packages
+function fileFilter(req, file, callback) {
+  const currentFileExt = path.extname(file.originalname)
+
+  if (!['.tgz', '.json'].includes(currentFileExt)) {
+    return callback(new Error(messages.PACKAGE_FILE_TYPE))
+  }
+
+  callback(null, true)
+}
 
 const processPackageBundle = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: MAX_FILE_SIZE },
-})
+  fileFilter,
+}).fields([
+  { name: 'packageBundle', maxCount: 1 },
+  { name: 'packageMeta', maxCount: 1 },
+])
 
 module.exports = processPackageBundle
